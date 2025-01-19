151dac052b1a88927a27427c3366ce87f80dc36a
TAKOALT <t.altunashvili@gmail.com>
2025-01-19 16:40:12 +0400

    initial commit

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -4,7 +4,7 @@ import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Davaleba {
+public class ApiTest {
 
     @Test
     public void getRequest() {
